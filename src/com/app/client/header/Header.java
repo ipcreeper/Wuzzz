d6c66c1edbbe5e36aaf21d923ac8ff47f65df576
Roman <ipcreeper@gmail.com>
2012-02-26 14:01:14 +0200

    Wuzzz.

--- src/com/app/client/header/Header.java
+++ src/com/app/client/header/Header.java
@@ -1,5 +1,9 @@
 package com.app.client.header;
 
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.Column;
+import com.google.gwt.user.cellview.client.TextColumn;
+import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 
@@ -16,5 +20,6 @@ public class Header extends HorizontalPanel {
 
         logo.setStyleName("logo");
         add(logo);
+
     }
 }
